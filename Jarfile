0e0311404e18629d94cbfa13c1e2f33104062af4
phoet <phoetmail@googlemail.com>
2014-03-14 13:24:20 -0400

    next try on travis

--- Jarfile
+++ Jarfile
@@ -1,5 +1,4 @@
 jar 'jline:jline:2.11'
-local "#{ENV['JAVA_HOME']}/lib/tools.jar"
 
 repository 'https://repository.cloudera.com/artifactory/cloudera-repos'
 jar 'org.apache.hadoop:hadoop-common:2.0.0-cdh4.4.0'
--- ext/mkrf_conf.rb
+++ ext/mkrf_conf.rb
@@ -1,5 +1,3 @@
 require 'lock_jar'
 jarfile  = File.expand_path("../Jarfile", File.dirname(__FILE__))
-lockfile = File.expand_path("../Jarfile.lock", File.dirname(__FILE__))
 LockJar.lock(jarfile)
-LockJar.install(lockfile)
