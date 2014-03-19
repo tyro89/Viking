2da7d9c7d7776adfd1b8174c93b203d46f5b3b00
Erik Selin <erik.selin@jadedpixel.com>
2014-03-19 14:41:03 -0400

    make the write buffer size configurable

--- lib/viking.rb
+++ lib/viking.rb
@@ -11,9 +11,12 @@ module Viking
   java_import org.apache.hadoop.fs.Path
   java_import org.apache.hadoop.fs.permission.FsPermission
 
+  DEFAULT_BUFFER_SIZE = 4096
+
   def self.configure(config)
-    hostname = config[:host]
-    port     = config[:port]
+    hostname     = config[:host]
+    port         = config[:port]
+    @buffer_size = config[:buffer_size]
 
     path = URI.new("hdfs://#{hostname}:#{port}")
 
@@ -23,6 +26,10 @@ module Viking
 
   def self.client
     @client ||= FileSystem.get_local(Configuration.new)
+  end
+
+  def self.buffer_size
+    @buffer_size || DEFAULT_BUFFER_SIZE
   end
 end
 
